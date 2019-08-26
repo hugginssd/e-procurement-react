3346601247649f6d539f5bcbb1b9b1b7444c253a
hugginssd <hugginsmafigu@gmail.com>
2019-08-26 09:32:05 +0200

    Updated exercise file(s)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Route, BrowserRouter as Router} from 'react-react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import LoginComponent from '../login/login';
+import SignupComponent from '../signup/signup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyChUmimdAXTxkDpj9UIt0zAm7D_FDvB-zU",
+    authDomain: "chat-app-21680.firebaseapp.com",
+    databaseURL: "https://chat-app-21680.firebaseio.com",
+    projectId: "chat-app-21680",
+    storageBucket: "chat-app-21680.appspot.com",
+    messagingSenderId: "347502776442",
+    appId: "1:347502776442:web:e909df12d5137eaf"
+});
+
+const routing = ( 
+        <Router >
+            <div id = "routing-container" >
+                <Route path = '/login' component = { LoginComponent } ></Route> 
+                <Route path = '/signup' component = { SignupComponent } ></Route> 
+            </div>  
+        </Router>
+    );
+    
+
+
+
+
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
